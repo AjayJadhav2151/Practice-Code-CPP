a05b03945f246827bd0e78757d05eea7d6c5a2fe
AjayJadhav2151 <ajayjadhav.skn.comp@gmail.com>
2025-03-13 23:49:53 +0530

    Adding Object oriented programming parctice Assignment 2

---
+++ oop2.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+class date {
+	int day;
+	int month;
+	int year;
+public:
+	void setdata(int d, int m, int y);
+	void showdate();
+	int getday();
+	int getmonth();
+	int getyear();
+};
+int date::getday() {
+	return day;
+}
+int date::getmonth() {
+	return month;
+}
+int date::getyear() {
+	return year;
+}
+void date::setdata(int d, int m, int y) {
+	day = d;
+	month = m;
+	year = y;
+}
+void date::showdate() {
+	cout << day << "/" << month << "/" << year << endl;
+}
+int main() {
+	date d;
+	d.setdata(8, 11, 2001);
+	d.showdate();
+}
